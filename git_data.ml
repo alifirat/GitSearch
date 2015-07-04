41ac1ea62db5b393e1dcd014b93a6eefa2bc69c3
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-07-04 12:42:11 +0200

    Add a data representing a commit and function in order to build a commit

--- git_data.ml
+++ git_data.ml
@@ -9,4 +9,12 @@ type github_directories = git_project_informations list
 let create_git_project_informations name full_name description =
   { name = name; full_name = full_name; description = description}
 
+type commit = {
+  author : string;
+  message : string;
+  time : string
+}
+
+let create_commit author message time =
+  {author = author; message = message; time = time}
 
--- git_data.mli
+++ git_data.mli
@@ -6,3 +6,12 @@ type git_project_informations = {
 
 val create_git_project_informations:
   string -> string -> string -> git_project_informations
+
+
+type commit = {
+  author : string;
+  message : string;
+  time : string;
+}
+
+val create_commit : string -> string -> string -> commit
