eefd326828b8cd6f32563919c81d94d3ffa6aa9d
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-07-04 09:40:04 +0200

    Adding functions displaying the github's project resulting from a search

--- js_client.ml
+++ js_client.ml
@@ -2,32 +2,35 @@ open Utils
 
 module Html = Dom_html
 
-let get_user_search id =
-  let input = get_element_by_id id in
-  Js.to_string (input##innerHTML)
+let display_git_project_stats li_id = 
+  Printf.printf "%s\n" (Js.to_string li_id)
 
-let show_github_projects button_search_id input_id =
-  let display_github_projects response = match response##readyState with
-    | XmlHttpRequest.DONE ->
-      if response##status = 200 then
-        let table = create_table "results" "project-results" in
-        let tr_description = create_tr table "description" in
-        create_td tr_description ["Name"; "Description"];
-        let l = Json_parser.extract_git_projects
-            (Js.to_string response##responseText) in 
-        List.iteri (fun i r -> 
-            let tr = create_tr table (string_of_int i) in 
-            let data = [r.Data.name; r.Data.description] in
-            create_td tr data) l;
-      else Printf.printf "error"
-    | _ -> () in
+let display_github_projects response = match response##readyState with
+  | XmlHttpRequest.DONE ->
+    if response##status = 200 then        
+      let ul = create_ul "results" "git_projects" in
+      let l = Json_parser.extract_git_projects
+          (Js.to_string response##responseText) in 
+      List.iter (fun r -> 
+          let data = ["Name = ";
+                      r.Data.name;
+                      " - Description = ";
+                      r.Data.description] in
+          let li = create_li ul r.Data.full_name data in
+          li##onclick <- Html.handler (fun _ ->
+              display_git_project_stats li##id;
+              Js._true)) l
+    else Printf.printf "error"
+  | _ -> ()
+
   
+let show_github_projects button_search_id input_id =  
     let button_search = get_element_by_id button_search_id in  
     button_search##onclick <- Html.handler (fun _ ->
         let user_search = input_value input_id in
         Printf.printf "user_search = %s\n" user_search;
-        let params = [("sort","stars"); ("order","desc")] in
-        let response = Http.http_request user_search params in
+        let data = [("q", user_search);("sort","stars"); ("order","desc")] in
+        let response = Http.search_git_project data in
         let callback () = display_github_projects response in 
         response##onreadystatechange <- Js.wrap_callback callback;
       Js._true)
--- js_client.mli
+++ js_client.mli
@@ -1,3 +1,1 @@
-val get_user_search: string -> string 
-
 val show_github_projects : string -> string -> unit 
