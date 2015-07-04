01f452e9b40feeb1bba396212548d03c7ff9fbb7
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-07-04 12:43:29 +0200

    Add a function extracting commits

--- json_parser.ml
+++ json_parser.ml
@@ -9,7 +9,7 @@ let extract_git_projects responseText =
       let name = obj |> member "name" |> to_string in 
       let full_name = obj |> member "full_name" |> to_string in
       let description = obj |> member "description" |> to_string in
-      Data.create_git_project_informations name full_name description)
+      Git_data.create_git_project_informations name full_name description)
 
 let get_contributors str =
   let json = Yojson.Basic.from_string str in
@@ -17,6 +17,31 @@ let get_contributors str =
   |> to_list
   |> List.map (fun obj -> obj |> member "login" |> to_string)
 
+let take l n =
+  let cut_list l limit = 
+    List.fold_left (fun (acc,n) commit ->
+        if commit.Git_data.author = "NULL"
+        then acc,n
+        else if n >= limit then acc,n+1
+        else commit::acc,n+1) ([], 0) l in
+  cut_list l n
+
+let get_commits str = 
+  let json = Yojson.Basic.from_string str in
+  let l =
+    json
+    |> to_list 
+    |> List.map (fun obj ->        
+        try
+          let commit = obj |> member "commit" in
+          let message = commit |> member "message" |> to_string in
+          let date = commit |> member "committer" |> member "date" |> to_string
+          in
+          let login = obj |> member "committer" |> member "login" |> to_string
+          in Git_data.create_commit login message date         
+        with Yojson.Basic.Util.Type_error _ ->
+          let null = "NULL" in Git_data.create_commit null null null) in   
+  fst (take l 100)
 
 
 
--- json_parser.mli
+++ json_parser.mli
@@ -1,3 +1,5 @@
-val extract_git_projects : string -> Data.git_project_informations list
+val extract_git_projects : string -> Git_data.git_project_informations list
 
-val get_contributors : string -> string list 
+val get_contributors : string -> string list
+
+val get_commits : string -> Git_data.commit list 
