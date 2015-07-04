b1fedd907181992f3d67077c8a6e92ecc14a546c
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-07-04 12:23:12 +0200

    Remove comments

--- main.ml
+++ main.ml
@@ -1,23 +1,6 @@
 open Utils
-
-(** [alert_with_response req] show the content of the request [req] 
-    in a window. *)
-let print_response req = match req##readyState with
-  | XmlHttpRequest.DONE ->
-    let status = req##status in
-    if status = 200
-    then Printf.printf "%s\n" (Js.to_string (req##responseText))
-    else Printf.printf "error"
-  | _ -> () 
-
            
-let () =
-  (* let params = [("sort","stars"); ("order","desc")] in  *)
-  (* let req = Http.search_github_project "tetris" params in *)
-  (* let callback () = print_response req in  *)
-  (* req##onreadystatechange <- Js.wrap_callback callback *)
-
-  
+let () =  
   let button_search_id = "user-search-validate" in
   let input_search_id = "user-search" in 
   Js_client.show_github_projects button_search_id input_search_id 
