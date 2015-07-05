87a723375dd270f4bbb4bc264785960d2750c268
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-07-05 22:18:38 +0200

    Add a new data representing the discrete bar chart

--- js_data.ml
+++ js_data.ml
@@ -1,12 +1,44 @@
-class type pieChart_data = object
-  method key : Js.js_string Js.t Js.prop
-  method y : int Js.prop
+class type discreteBar_item = object
+  method label : Js.js_string Js.t Js.prop
+  method value : float Js.prop
 end
 
-let empty_pieChart_data () : pieChart_data Js.t = Js.Unsafe.obj [| |]
+let empty_discreteBar_item () : discreteBar_item Js.t = Js.Unsafe.obj [| |]
 
-let create_pieChart_data label value =
-  let data = empty_pieChart_data () in
-  data##key <- Js.string label;
-  data##y <- value;
+let create_discreteBar_item label value =
+  let data = empty_discreteBar_item () in
+  data##label <- Js.string label;
+  data##value <- value;
   data
+
+class type discreteBar_chart = object
+  method key: Js.js_string Js.t Js.prop
+  method values : discreteBar_item Js.t Js.js_array Js.t Js.prop 
+end
+
+let empty_discreteBar_chart () : discreteBar_chart Js.t = Js.Unsafe.obj [| |]    
+
+let create_discreteBar_chart key values = 
+  let chart = empty_discreteBar_chart () in
+  chart##key <- Js.string key;
+  chart##values <- values;
+  chart
+
+
+class type timeline_data = object
+  method id : int Js.prop
+  method content : Js.js_string Js.t Js.prop
+  method start: Js.js_string Js.t Js.prop
+end
+
+
+let empty_timeline_data () : timeline_data Js.t = Js.Unsafe.obj [| |]
+
+let create_timeline_data id content start =
+  let data = empty_timeline_data () in
+  data##id <- id;
+  data##content <- Js.string content;
+  data##start <- Js.string start;
+  data
+  
+
--- js_data.mli
+++ js_data.mli
@@ -1,8 +1,24 @@
-class type pieChart_data = object
-  method key : Js.js_string Js.t Js.prop
-  method y : int Js.prop
+class type discreteBar_item = object
+  method label : Js.js_string Js.t Js.prop
+  method value : float Js.prop
+end
+   
+val create_discreteBar_item : string -> float -> discreteBar_item Js.t
+
+class type discreteBar_chart = object
+  method key: Js.js_string Js.t Js.prop
+  method values : discreteBar_item Js.t Js.js_array Js.t Js.prop 
 end
 
-    
-val create_pieChart_data : string -> int -> pieChart_data Js.t
+val create_discreteBar_chart:string ->
+  discreteBar_item Js.t Js.js_array Js.t -> 
+  discreteBar_chart Js.t
+  
+class type timeline_data = object
+  method id : int Js.prop
+  method content : Js.js_string Js.t Js.prop
+  method start: Js.js_string Js.t Js.prop
+end
+
+val create_timeline_data : int -> string -> string -> timeline_data Js.t
 
