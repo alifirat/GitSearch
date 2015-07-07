ce38af0e600979b1f2f09dafab67a6616420a712
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-07-08 00:53:12 +0200

    Make more generic the function concat_list and add documentation

--- http.ml
+++ http.ml
@@ -1,30 +1,32 @@
 open Utils
 
+(* Global github api url (hope that doesn't change!) *)
 let github_api_url = "https://api.github.com/"
-  
-let concat_parameters data =
-  List.map (fun (field,value) -> field ^ "=" ^ value) data
+
+(* [concat_list data sep] return the concatenation of every element in [data] 
+   separated by [sep]. *) 
+let concat_list data sep =
+  List.map (fun (field,value) -> field ^ sep ^ value) data
   |> String.concat "&"
 
 let http_request suffix data =
   let response = XmlHttpRequest.create () in
-  let url = github_api_url ^ suffix ^ (concat_parameters data) in
+  let url = github_api_url ^ suffix ^ (concat_list data  "=") in
   response##_open(Js.string "GET", Js.string url, Js.bool true);
   response##send (Js.some (Js.string ""));
   response
 
 let search_git_project data = http_request "search/repositories?" data
 
-let search_committers full_name =
+let search_committers name =
   http_request
-    ("repos/" ^ full_name ^ "/contributors")
+    ("repos/" ^ name ^ "/contributors")
     []
     
-let search_commits full_name =
+let search_commits name =
   http_request
-    ("repos/" ^ full_name ^ "/commits?")
-    [("per_page","100"); ("until", Date.current_date())
-    ] 
+    ("repos/" ^ name ^ "/commits?")
+    [("per_page","100"); ("until", Date.current_date())] 
 
   
 
--- http.mli
+++ http.mli
@@ -1,11 +1,12 @@
-val http_request: 
-  string ->
-  (string * string) list ->
-  XmlHttpRequest.xmlHttpRequest Js.t
+(** [search_git_project data] send a request corresponding to a search of a 
+    GitHub project (contains in [data]) and return the resulting response of the 
+    server. *)
+val search_git_project:(string*string) list->XmlHttpRequest.xmlHttpRequest Js.t
 
-val search_git_project:
-  (string * string) list -> XmlHttpRequest.xmlHttpRequest Js.t
-
+(** [search_commiters name] return the resulting response from the server 
+    to the request getting all committers of the project [name]. *)
 val search_committers : string -> XmlHttpRequest.xmlHttpRequest Js.t
 
+(** [search_commits name] return the resulting response from the server 
+    to the request getting the last 100 commits of the project [name] *)
 val search_commits : string ->  XmlHttpRequest.xmlHttpRequest Js.t
