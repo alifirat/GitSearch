f130c42a1d7f844bc97284ea38b0bffa8103e9fd
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-07-09 17:49:17 +0200

    Documentation about the data that we get from the GitHub Api

--- git_data.ml
+++ git_data.ml
@@ -6,8 +6,8 @@ type git_project_informations = {
 
 type github_directories = git_project_informations list
 
-let create_git_project_informations name full_name description =
-  { name = name; full_name = full_name; description = description}
+let create_git_project_informations name full_name desc =
+  { name = name; full_name = full_name; description = desc}
 
 type commit = {
   author : string;
@@ -15,6 +15,6 @@ type commit = {
   time : string
 }
 
-let create_commit author message time =
-  {author = author; message = message; time = time}
+let create_commit author m d =
+  {author = author; message = m; time = d}
 
--- git_data.mli
+++ git_data.mli
@@ -1,17 +1,26 @@
+(** This module represents the data that we get from the GitHub Api. *)
+
+(** Catch the information that we want from a GitHub project.*)
 type git_project_informations = {
   name: string;
   full_name : string;
   description : string
 }
 
+(** [create_git_project_informations name full_name desc] return 
+    the GitHub project [name], having the description [desc] and the fullname
+    [full_name]. *)
 val create_git_project_informations:
   string -> string -> string -> git_project_informations
 
-
+(** For a commit, we deal only with his author, the his and the date of
+    his date *)
 type commit = {
   author : string;
   message : string;
   time : string;
 }
 
+(** [create_commit author m d] return the commit created by [author],
+ with the message [m] at the date [d].*)
 val create_commit : string -> string -> string -> commit
