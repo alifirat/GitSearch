5a52a959f711f022238b616ebdd7b164e219f9a0
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-07-14 14:46:33 +0200

    Rename some functions interfaces

--- src/js_client_ui.mli
+++ src/js_client_ui.mli
@@ -2,13 +2,10 @@
     the kind of stats to show in the <ul> element having the id [ul_id] .*)
 val create_menu_tabs: string -> unit 
 
-(** [add_favorite_button div_id id content] add a favorite button in the
-    div having the id [div_id] with the textContent [content]. *)
-val add_favorite_button : string -> string -> string -> unit
 
-(** [add_project_title div_id id title] display the [title] of a GitHub project 
-    in the div with the id [div_id].*)
-val add_project_title : string -> string -> string -> unit
+(** [set_project_title id title] set the title of the GitHub project's 
+    title to [title]. *)
+val set_project_title : string -> string -> unit
 
 (** [show_element id] make visible the html element having for id [id]. *)
 val show_element : string -> unit
